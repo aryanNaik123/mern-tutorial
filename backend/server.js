b478a7be12f49393e814d33fb3f1f21054a8adf5
Aryan Naik <66840062+aryanNaik123@users.noreply.github.com>
2023-03-07 20:05:30 -0500

    finished part 1

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const { ErrorHandler, errorHandler } = require("./middleware/errorMiddleware");
+connectDB();
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
